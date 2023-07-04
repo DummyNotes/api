52189448bb6a9c0b802d77aa2d6b01bf28b6e98a
tiborhercz <tibor@hercz.nl>
2023-07-04 11:54:52 +0200

    remove middleware from health check endpoint

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"github.com/dummynotes/notes/internal/middleware"
 	"log"
 	"net/http"
 
 	"github.com/dummynotes/notes/internal/config"
 	"github.com/dummynotes/notes/internal/database"
 	"github.com/dummynotes/notes/internal/handlers"
-	"github.com/dummynotes/notes/internal/middleware"
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 )
@@ -22,13 +22,15 @@ func main() {
 
 	router := gin.Default()
 	router.Use(gzip.Gzip(gzip.DefaultCompression))
-	router.Use(middleware.ExtractPayload())
 
-	router.GET("/health", func(c *gin.Context) {
+	public := router.Group("/")
+
+	public.GET("/health", func(c *gin.Context) {
 		c.Status(http.StatusOK)
 	})
 
 	notesGroup := router.Group("/notes")
+	notesGroup.Use(middleware.ExtractPayload())
 
 	notesGroup.GET("/logheaders", func(c *gin.Context) {
 		for k, v := range c.Request.Header {
